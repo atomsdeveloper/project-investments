dfe2820535fe5fad704e38e277b2540cf212b486
renanns <atomsfrontenf@gmail.com>
2024-07-12 17:41:00 -0300

    chore: add settings to test and compile files jsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './template/App';
+import { App } from './template/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/template/App.jsx
@@ -0,0 +1,5 @@
+import * as Styled from './styles';
+
+export const App = () => {
+  return <Styled.Container>Testando o componente App</Styled.Container>;
+};
