0ba2d138ccefee607ab7a5c6f00aab279c2c303c
renanns <atomsfrontenf@gmail.com>
2024-07-12 18:19:53 -0300

    fix: remove dependencie storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:storybook/recommended',
   ],
   globals: {
     Atomics: 'readonly',
